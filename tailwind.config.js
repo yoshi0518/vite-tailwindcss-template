350d92e1cbf830a41cf7c39303ca3381a707d84a
yoshi0518 <yoshihumi.n.0518@gmail.com>
2023-03-30 22:42:41 +0900

    構成見直、watchコマンド追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./**/*.{html,css,js}'],
+  content: [
+    // './**/*.{html,css,js}',
+    'index.html',
+  ],
   theme: {
     extend: {},
   },
